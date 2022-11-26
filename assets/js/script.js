e5da3f6e98d27a4ca564d1a8fe590bd704ea36dc
unikforceit <freelancerakibul@gmail.com>
2022-11-27 00:09:50 +0600

    jquery added

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// alert('Hey I am here!');
+// $(".form-title").hide();
+// $("body").css("background-color", "#ccc");
+// $(".form-title").show();
+// console.log('I am here');
+// function myfunction(){
+//     alert('Hey I am here!');
+// }
+// $(document).ready(myfunction());
+// $(".form-title").on("mouseenter", function(){
+//   $("body").css("background-color", "#ccc");
+// });
+// $(".form-title").on("mouseleave", function(){
+//     $("body").css("background-color", "#fff");
+//   });
+// $(".form-title").on("click", function(){
+//     console.log("clicked");
+//     $(".form-wrapper").slideToggle();
+// });
